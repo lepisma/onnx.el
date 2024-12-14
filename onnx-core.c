bb1a5146d376474a5dc018a3de311762bb5f2bff
Abhinav Tushar <abhinav@lepisma.xyz>
2024-12-14 14:13:34 +0530

    Raise errors in Emacs instead of aborting the program

--- onnx-core.c
+++ onnx-core.c
@@ -8,33 +8,38 @@ int plugin_is_GPL_compatible;
 const char* onnx_core_version = "0.0.1";
 
 const OrtApi* g_ort = NULL;
+emacs_env *g_env = NULL;
 
-// TODO: Use Emacs non local exit signal
-#define ORT_ABORT_ON_ERROR(expr)                                \
+void emacs_signal_error(emacs_env *env, const char* code, const char* message) {
+  emacs_value err_sym = env->intern(env, code);
+  emacs_value data = env->make_string(env, message, strlen(message));
+  env->non_local_exit_signal(env, err_sym, data);
+}
+
+#define ORT_RAISE_ON_ERROR(expr)                                \
   do {                                                          \
     OrtStatus* onnx_status = (expr);                            \
     if (onnx_status != NULL) {                                  \
       const char* msg = g_ort->GetErrorMessage(onnx_status);    \
-      fprintf(stderr, ":: %s\n", msg);                          \
+      emacs_signal_error(g_env, "onnx-error", msg);             \
       g_ort->ReleaseStatus(onnx_status);                        \
-      abort();                                                  \
     }                                                           \
   } while (0);
 
-
-int run_inference(OrtSession* session, float* input, size_t input_size, float* output, size_t output_size) {
-  int ret = 0;
-  // TODO: Do model execution
-  return ret;
+void run_inference(OrtSession* session, float* input, size_t input_size, float* output, size_t output_size) {
 }
 
-// For now we will only work with single input and output models
+// For now we only work with single input and output models
 void verify_input_output_count(OrtSession* session) {
   size_t count;
-  ORT_ABORT_ON_ERROR(g_ort->SessionGetInputCount(session, &count));
-  assert(count == 1);
-  ORT_ABORT_ON_ERROR(g_ort->SessionGetOutputCount(session, &count));
-  assert(count == 1);
+  ORT_RAISE_ON_ERROR(g_ort->SessionGetInputCount(session, &count));
+  if (count != 1) {
+    emacs_signal_error(g_env, "onnx-model-error", "Model input count is not 1");
+  }
+  ORT_RAISE_ON_ERROR(g_ort->SessionGetOutputCount(session, &count));
+  if (count != 1) {
+    emacs_signal_error(g_env, "onnx-model-error", "Model output count is not 1");
+  }
 }
 
 struct model_t {
@@ -44,27 +49,34 @@ struct model_t {
 };
 
 struct model_t load_model(ORTCHAR_T* model_path) {
-  g_ort = OrtGetApiBase()->GetApi(ORT_API_VERSION);
-  /* if (!g_ort) { */
-  /*   fprintf(stderr, "Failed to init ONNX Runtime engine.\n"); */
-  /* } */
+  struct model_t model;
+
+  if (!g_ort) {
+    g_ort = OrtGetApiBase()->GetApi(ORT_API_VERSION);
+    if (!g_ort) {
+      emacs_signal_error(g_env, "onnx-error", "Failed to initialize ORT API");
+      return model;
+    }
+  }
 
   ORTCHAR_T* execution_provider = "cpu";
   OrtEnv* env;
 
-  ORT_ABORT_ON_ERROR(g_ort->CreateEnv(ORT_LOGGING_LEVEL_WARNING, "test", &env));
-  assert(env != NULL);
+  ORT_RAISE_ON_ERROR(g_ort->CreateEnv(ORT_LOGGING_LEVEL_WARNING, "test", &env));
+  if (env == NULL) {
+    emacs_signal_error(g_env, "onnx-error", "Failed to create environment");
+    return model;
+  }
 
   OrtSessionOptions* session_options;
-  ORT_ABORT_ON_ERROR(g_ort->CreateSessionOptions(&session_options));
+  ORT_RAISE_ON_ERROR(g_ort->CreateSessionOptions(&session_options));
 
   OrtSession* session;
-  ORT_ABORT_ON_ERROR(g_ort->CreateSession(env, model_path, session_options, &session));
+  ORT_RAISE_ON_ERROR(g_ort->CreateSession(env, model_path, session_options, &session));
 
   // TODO: Enable check
   // verify_input_output_count(session);
 
-  struct model_t model;
   model.env = env;
   model.session_options = session_options;
   model.session = session;
@@ -91,13 +103,15 @@ emacs_value Fonnx_load_model(emacs_env* env, ptrdiff_t n, emacs_value args[], vo
   env->copy_string_contents(env, args[0], NULL, &len);
   char* buf = malloc(len);
   if (!buf) {
-    // TODO
+    emacs_signal_error(g_env, "onnx-memory-error", "Failed to allocate for filepath string");
+    return NULL;
   }
   env->copy_string_contents(env, args[0], buf, &len);
 
   struct model_t* model = malloc(sizeof(struct model_t));
   if (!model) {
-    // TODO
+    emacs_signal_error(g_env, "onnx-memory-error", "Failed to allocate for model");
+    return NULL;
   }
   *model = load_model(buf);
   free(buf);
@@ -113,20 +127,20 @@ int emacs_module_init(struct emacs_runtime* runtime) {
   if (runtime->size < sizeof(*runtime))
     return 1;
 
-  emacs_env* env = runtime->get_environment(runtime);
-  if (env->size < sizeof(*env))
+  g_env = runtime->get_environment(runtime);
+  if (g_env->size < sizeof(*g_env))
     return 2;
 
-  emacs_value version_fn = env->make_function(env, 0, 0, Fonnx_core_version, "Return version of onnx-core.", NULL);
-  emacs_value version_fn_args[] = {env->intern(env, "onnx-core-version"), version_fn};
-  env->funcall(env, env->intern(env, "defalias"), 2, version_fn_args);
+  emacs_value version_fn = g_env->make_function(g_env, 0, 0, Fonnx_core_version, "Return version of onnx-core.", NULL);
+  emacs_value version_fn_args[] = {g_env->intern(g_env, "onnx-core-version"), version_fn};
+  g_env->funcall(g_env, g_env->intern(g_env, "defalias"), 2, version_fn_args);
 
-  emacs_value load_fn = env->make_function(env, 1, 1, Fonnx_load_model, "Load given ONNX model file.", NULL);
-  emacs_value load_fn_args[] = {env->intern(env, "onnx-core-load-model"), load_fn};
-  env->funcall(env, env->intern(env, "defalias"), 2, load_fn_args);
+  emacs_value load_fn = g_env->make_function(g_env, 1, 1, Fonnx_load_model, "Load given ONNX model file.", NULL);
+  emacs_value load_fn_args[] = {g_env->intern(g_env, "onnx-core-load-model"), load_fn};
+  g_env->funcall(g_env, g_env->intern(g_env, "defalias"), 2, load_fn_args);
 
-  emacs_value provide_fn_args[] = {env->intern(env, "onnx-core")};
-  env->funcall(env, env->intern(env, "provide"), 1, provide_fn_args);
+  emacs_value provide_fn_args[] = {g_env->intern(g_env, "onnx-core")};
+  g_env->funcall(g_env, g_env->intern(g_env, "provide"), 1, provide_fn_args);
 
   return 0;
 }
